b6f84b7f80af209e17c8d21451ce9f8159711673
Lxtharia <maud-lin@gmx.de>
2025-06-23 18:58:29 +0200

    Migrate to ffmpeg_the_third v3.0.2

--- src/lib.rs
+++ src/lib.rs
@@ -18,7 +18,7 @@ use egui::{
     vec2, Align2, Color32, ColorImage, FontId, Image, Pos2, Rect, Response, Rounding, Sense,
     Spinner, TextureHandle, TextureOptions, Ui, Vec2,
 };
-use ffmpeg::error::EAGAIN;
+use libc::EAGAIN;
 use ffmpeg::ffi::{AVERROR, AV_TIME_BASE};
 use ffmpeg::format::context::input::Input;
 use ffmpeg::format::{input, Pixel};
@@ -1262,7 +1262,7 @@ pub trait Streamer: Send {
             let target_ts = millisec_to_timestamp(target_ms, rescale::TIME_BASE);
 
             // TODO: propogate error
-            if self.input_context().seek(target_ts, ..target_ts).is_ok() {
+            if self.input_context().seek(target_ts, ..=target_ts).is_ok() {
                 self.decoder().flush();
                 let mut previous_elapsed_ms = self.elapsed_ms().get();
 
@@ -1358,7 +1358,7 @@ pub trait Streamer: Send {
     fn reset(&mut self) {
         let beginning: i64 = 0;
         let beginning_seek = beginning.rescale((1, 1), rescale::TIME_BASE);
-        let _ = self.input_context().seek(beginning_seek, ..beginning_seek);
+        let _ = self.input_context().seek(beginning_seek, ..=beginning_seek);
         self.decoder().flush();
     }
     /// Keep recieving packets until a frame can be decoded.
